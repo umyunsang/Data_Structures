652ca58bc55aa364e2f95be63f54fc7ba8fc1c45
UmYunSang <dbstkd5865@gmail.com>
2024-05-16 16:25:24 +0900

    AVL

--- AVI/AVL.py
+++ AVI/AVL.py
@@ -1,13 +1,10 @@
 from collections import deque
-
-
 class AVLNode:
     def __init__(self, data):
         self.data = data
-        self.right = None
         self.left = None
+        self.right = None
         self.height = 1
-
 
 class AVLTree:
     def get_height(self, node):
@@ -18,63 +15,87 @@ class AVLTree:
     def get_balance(self, node):
         if not node:
             return 0
-        return self.get_height(node.left) - self.get_height(node.right)
+        bf = self.get_height(node.left) - self.get_height(node.right)
+        print("balance: ", bf)
+        return bf
 
-    #left rotate
+    def rotate_right(self, y):
+        x = y.left
+        T2 = x.right
+
+        # Perform rotation
+        x.right = y
+        y.left = T2
+
+        # Update heights
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+
+        # Return new root
+        return x
+
     def rotate_left(self, x):
         y = x.right
         T2 = y.left
 
+        # Perform rotation
         y.left = x
         x.right = T2
 
-        y.left = x
-        x.right = T2
-
+        # Update heights
         x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
         y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
 
+        # Return new root
         return y
 
     def insert(self, node, data):
+        # Perform normal BST insertion
         if not node:
-            return AVLNode(data)
+            newNode = AVLNode(data)
+            print(f"new node data: ", newNode.data)
+            print(f"new node height: ", newNode.height)
+            print()
+            return newNode
         elif data < node.data:
             node.left = self.insert(node.left, data)
         else:
             node.right = self.insert(node.right, data)
 
-        #update height
+        # Update height of this ancestor node
         node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
 
-        #calculate balance Factor
+        print(f"node data: ", node.data)
+        print(f"update node height: ", node.height)
+        print()
+
+
+        # Get the balance factor
         balance = self.get_balance(node)
 
-        #left left case
-        if balance > 1 and data < node.left.data:
-            pass
+        # If node becomes unbalanced, then 4 cases arise
 
-        #right right
+        # Left Left Case
+        if balance > 1 and data < node.left.data:
+            return self.rotate_right(node)
+
+        # Right Right Case
         if balance < -1 and data > node.right.data:
             return self.rotate_left(node)
 
-        #left right
+        # Left Right Case
         if balance > 1 and data > node.left.data:
-            pass
+            node.left = self.rotate_left(node.left)
+            return self.rotate_right(node)
 
-        #right left
+        # Right Left Case
         if balance < -1 and data < node.right.data:
-            pass
+            node.right = self.rotate_right(node.right)
+            return self.rotate_left(node)
 
         return node
 
-    def inorder(self, node):
-        if node:
-            self.inorder(node.left)
-            print(node.data, end=" ")
-            self.inorder(node.right)
-
-    def levelOrder(self, root):
+    def level_order(self, root):
         if root is None:
             return 0
 
@@ -87,7 +108,7 @@ class AVLTree:
             print(f"★ LEVEL ", cnt, ": ", end="")
             for _ in range(size):
                 node = queue.popleft()
-                print(node.data, end=" ")
+                print("{0} ".format(node.data), end="")
 
                 if node.left:
                     queue.append(node.left)
@@ -96,3 +117,18 @@ class AVLTree:
             print()
 
         return
+
+    def pre_order(self, node):
+        if not node:
+            return
+        print("{0} ".format(node.data), end="")
+        self.pre_order(node.left)
+        self.pre_order(node.right)
+
+    def in_order(self,node):
+        if not node:
+            return
+        if node:
+            self.in_order(node.left)
+            print("{0} ".format(node.data), end="")
+            self.in_order(node.right)
